82aee834091264cdd0598a7f7c08032e05e3926e
Rasel Kabir <md.rasel@technonext.com>
2025-12-25 20:40:18 +0600

    arrow function and pass paramitter

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import logo from './logo.svg';
 function App() {
   return (
     <div className="App">
-      <Clock locale="bn-BD" />
+      <Clock/>
     </div>
   );
 }
--- src/Components/Clock.js
+++ src/Components/Clock.js
@@ -1,24 +1,31 @@
 import React from "react";
-
+//#8 React Event Handling & Control Re Rendering - React Tutorial Bangla Series
 class Clock extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { date: new Date(),
-
-         };
-    }
+    state = { date: new Date(), local: 'en-US' };
     componentDidMount() {
-        setInterval(() => 
-            this.setState({ date: new Date() }), 
-        1000);
+        this.clockTimer = setInterval(() =>
+            this.tick(), 1000);
+    }
+    componentWillUnmount() {
+        clearInterval(this.clockTimer);
+    }
+    tick() {
+        this.setState({ date: new Date() });
+    }
+    handleClick = (locle) => {
+        console.log("Clock Clicked!");
+        this.setState({local: locle});
     }
 
     render() {
         return (
-            <h1 className="heading">
-                <span className="clock-text">The current time is {this.state.date
-                .toLocaleTimeString(this.props.locale)}.</span>
-            </h1>
+            <div>
+                <h1 className="heading">
+                    <span className="clock-text">The current time is {this.state.date
+                        .toLocaleTimeString(this.state.local)}.</span>
+                </h1>
+                <button onClick={() => this.handleClick('bn-BD')}>Click Me</button>
+            </div>
         );
     }
 }
